0313b0d533feb835d9e70c24856f00b34b81ba18
Lucas <pedrolucas994234430@gmail.com>
2025-12-14 18:06:39 +0000

    final pagamento

--- tasks/poo/pagamento/py/draft.py
+++ tasks/poo/pagamento/py/draft.py
@@ -4,27 +4,26 @@ class Pagamento(ABC):
     def __init__(self, valor: float, descricao: str):
         self.valor = valor
         self.descricao = descricao
-    
-    @abstractmethod
-    def validar_valor(self) -> None:
-        if self.valor  <= 0:
-            raise ValueErro("valor negativo")
-    
+     
     def resumo(self):
-        return f"Pagamento de R$ <valor>: <descricao>"
+        print(f"Pagamento de R$ {self.valor}: {self.descricao}")
+
+    def validar_valor(self) -> None:
+        if self.valor <= 0:
+            raise ValueError("Valor invalido")
     
     @abstractmethod
-    def processar(self):
+    def realizar_pagamento(self):
         pass
-
+ 
 class Pix(Pagamento):
     def __init__(self, valor: float, descricao: str, chave: str, banco: str):
-        super.__init__(valor, descricao)
+        super().__init__(valor, descricao)
         self.chave = chave
         self.banco = banco
     
     def realizar_pagamento(self):
-        return f"Pagando pix produto{self.descricao} para {self.chave} do banco {self.banco} no valor {self.valor}"
+        print(f"Pix enviado via banco {self.banco}, usando chave {self.chave}.")
 
 class Cartao(Pagamento):
     def __init__(self,numero: str, nome: str, valor: float, descricao: str, limite_disponivel: float):
@@ -35,10 +34,24 @@ class Cartao(Pagamento):
 
     def realizar_pagamento(self):
         if self.valor > self.limite_disponivel:
-            raise Exception(f"Erro: Limite insuficiente")
+            print("Erro: Limite insuficiente")
         else:
-            limite_disponivel -= self.valor
-            return "Pagamento concluido"
+            self.limite_disponivel -= self.valor
+            print("Pagamento concluido")
+
+class Boleto(Pagamento):
+    def __init__(self, valor, descricao, codigo_barras, vencimento):
+        super().__init__(valor, descricao)
+        self.codigo_barras = codigo_barras
+        self.vencimento = vencimento
+
+    def realizar_pagamento(self):
+        print("Boleto gerado. Aguardando pagamento...")
+
+def realizar_pagamento(pagamento: Pagamento):
+    pagamento.validar_valor()
+    print(pagamento.resumo())
+    pagamento.realizar_pagamento()
 
 
     
